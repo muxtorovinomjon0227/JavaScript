586672f17cd1bf498f422cc2c7bdce1660fac2de
Inomjon2000 <muxtorovinomjon0227@gmail.com>
2024-01-16 20:56:46 +0500

    Lesson 1

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -27,5 +27,170 @@
 // console.log(10+3);
 // console.log(5/2);
 
+// console.log("Basic Print method in JavaScript");
 
+// // Variable declaration
+// let c, d, e;
+
+// // Assign value to the variable
+// c = 5; 
+
+// // Computer value of variables
+// d = c;
+// e = c/d;
+
+// console.log(c,d,e,c)
+
+
+///// ========= JavaScript Veribles
+
+// // Declare a variable and initialize it
+// // Global variable declaration
+// let Name = "Apple";
+ 
+// // Function definition
+// function MyFunction() {
+ 
+//     // Local variable declaration
+//     let num = 45;
+ 
+//     // Display the value of Global variable
+//     console.log(Name);
+ 
+//     // Display the value of local variable
+//     console.log(num);
+// }
+ 
+//// Function cal
+// MyFunction();  /// Funcsiya ichida bo'lmagan variblelar ishlamaydi 
+
+// /// =============== JavaScript Operators
+// let x,y,sum;
+// x = 3;
+// y = 4;
+// sum = x+y;
+// console.log(sum);
+
+// // Variable Declarations
+// let x, y, sum;
+
+// // Assign value to the variables
+// x = 3;
+// y = 23;
+
+// // Use arithmetic operator to
+// // add two numbers
+// sum = x + y;
+
+// console.log(sum);
+
+
+
+// /// =========== JavaScript Expression
+
+// let x, num, sum;
+// x = 20;
+// y = 30;
+// num = x/2;
+// sum = x + y;
+// console.log(num + "<br>" + sum);
+
+// //// ====-=== JavaScript Keyword
+
+// // let is the keyword used to
+// // define the variable
+// let a, b;
+// // function is the keyword which tells
+// // the browser to create a function
+// function GFG(){};
+
+
+/// ==========  JavaScript Comments 
+
+// let Comments = "Comments" 
+
+
+/// ================ JavaScript Functions
+
+// // Function definition
+// function func() {
+// 	// Declare a variable
+// 	let num = 45;
+// 	// Display the result
+// 	console.log(num);
+// }
+// // Function call
+// func();
+
+
+
+// let geek = "Computer scines portal";
+// console.log(geek);
+
+// let sum = 5 +8;
+// console.log(sum);
+
+
+ /// ============ Multi-line Comments
+
+ /*    
+This is a multiline comment   
+It can span multiple lines    
+*/
+
+// function func(){
+//     let x = 2;
+//     let y = 4;
+//     let z = x + y;
+//     console.log(z);
+// }
+// func();
+
+// function sub(){
+//     let x = 10;
+//     let y = 20;
+//     let z = x-y;
+//     console.log(z);
+// }
+// sub();
+
+
+
+// var:
+// Declares a variable. It has a function-scoped or globally-scoped behavior.
+// var a = "Hello Geeks";
+// var b = 10;
+// var c = 12;
+// var d = b +c;
+// console.log(a);
+// console.log(b);
+// console.log(c)
+// console.log(d);
+
+// let:
+// Introduces block-scoped variables. It’s commonly used for variables that may change their value.
+
+// let a = "Hello learners";
+// let b = "joining";
+// let c = " 12";
+// let d = b+c;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+// const:
+// Declares variables that cannot be reassigned. It’s block-scoped as well.
+
+
+    // const a = "Hello learns";
+    // console.log(a);
+
+    // const b = 400;
+    // console.log(b);
+
+    // const c = "12";
+    // console.log(c);
 

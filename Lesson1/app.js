71c7aee10a887165a7a1a18d082d2e81468dd840
Inomjon2000 <muxtorovinomjon0227@gmail.com>
2024-01-16 18:51:34 +0500

    Lesson 1

--- Lesson1/app.js
+++ Lesson1/app.js
@@ -14,10 +14,18 @@
 // b = 3;
 // c = a + b;
 // console.log("The value of c is " + c + ".");
-
 // function myFunction(){
 //     console.log("Hello JavaScript")
 //     console.log("Hello")
 // }
-
 // myFunction();
+
+// console.log(10*2);
+// console.log(30*2);
+// console.log(10-2);
+// console.log(2-5);
+// console.log(10+3);
+// console.log(5/2);
+
+
+

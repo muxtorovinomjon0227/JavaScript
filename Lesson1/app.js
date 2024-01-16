efc655b91b642104d67265c88a6cff086e8d2e9f
Inomjon2000 <muxtorovinomjon0227@gmail.com>
2024-01-16 18:45:07 +0500

    Lesson 1

---
+++ Lesson1/app.js
@@ -0,0 +1,23 @@
+// console.log("JavaScript language");
+
+
+// let a,b,c
+// a= 3,
+// b= 4,
+// c=5;
+// num = a+b+c;
+// console.log(a,b,c,num)
+
+
+// let a, b, c;
+// a = 2;
+// b = 3;
+// c = a + b;
+// console.log("The value of c is " + c + ".");
+
+// function myFunction(){
+//     console.log("Hello JavaScript")
+//     console.log("Hello")
+// }
+
+// myFunction();
